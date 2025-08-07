88c79661bda67e874e8f010dcd0bc7ed52ddb9b8
Jeancome24 <jeancome.maziere@gmail.com>
2025-08-07 16:33:25 -0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import InventoryExplorer from './InventoryExplorer';
+import InventoryExplorer from './InventoryExplorer.js';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
